418780359ba8df85b59012a2bfd7e6d64d1d5eed
Miranda Chiu <mirandachiu13@gmail.com>
2020-02-12 10:28:01 -0500

    adding knobs

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs/react';
 import { Button } from './Button';
 
 
 export default {
   title: 'Components|Button',
   component: Button,
+  decorators: [withKnobs],
 };
 
-export const Text = () => <Button>Hello Button</Button>;
+export const Text = () => <Button>{text("Text", "Hello Button")}</Button>;
 
 export const Emoji = () => (
   <Button onClick={action('clicked')}>
