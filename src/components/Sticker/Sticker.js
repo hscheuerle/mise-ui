8bae98589bb2444e4b6d09e71e0eda0468dc3525
Meg Broughton <meg.broughton@americastestkitchen.com>
2020-02-12 16:46:01 -0500

    Sticker tweak and docs formatting

--- src/components/Sticker/Sticker.js
+++ src/components/Sticker/Sticker.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { color, font, fontSize, letterSpacing, spacing } from '../../styles';
 
-const stickerHeight = '1.2rem';
+const stickerHeight = '1.5rem';
 
 const StyledSticker = styled.span`
   background-color: ${props => props.isPriority ? color.tomato : color.black};
   border-radius: 0.5rem;
   color: ${color.white};
   display: inline-block;
-  font: ${fontSize.xxsm}/${stickerHeight} ${font.pnb};
+  font: ${fontSize.xsm}/${stickerHeight} ${font.pnb};
   height: ${stickerHeight};
   letter-spacing: ${letterSpacing.sm};
   margin-bottom: ${spacing.xsm};
@@ -22,24 +22,40 @@ const StyledSticker = styled.span`
 `;
 
 /**
-  * A sticker surfaces ‘make or break’ information in order to help the user
+
+  A sticker surfaces ‘make or break’ information in order to help the user
   make a smarter decision between cards in the grid. Examples: “vegetarian” or
   “gluten free” or “popular” “editor’s pick”
 
-  * It is not interactive, only informative, so there is no hover state
+<br>
 
-  * There are two types of stickers, priority and basic. Priority stickers are
-  * reserved for messages like “new” or “price drop”, and have a red (`tomato`)
-  * background. Basic stickers have a `transparentBlack` background.
+-----
 
-  * It can have or not have an icon (collections icon, video icon)
+<br>
 
-  * A desktop card can have two stickers, max; one sticker max on mobile
+### Notes
 
-  * Text does not wrap
+Stickers are not interactive, only informative, so there is no hover state.
 
-  * Line height of the text and the height of pill should both be set to 1.2rem to vertically center text.
-  */
+There are two types of stickers, priority and basic. Basic stickers have a `transparentBlack` background. Priority stickers are reserved for messages like “new” or “price drop”, and have a red (`tomato`) background.
+
+It can have or not have an icon (collections icon, video icon).
+
+A desktop card can have two stickers, max; one sticker max on mobile
+
+Text does not wrap
+
+Line height of the text and the height of pill should both be set to `1.5rem` to vertically center text.
+
+Stickers scale down to a smaller size (`height: 1.2rem;` `font-size: 0.8rem;`) for mobile for most types of cards.
+
+<br>
+
+-----
+
+<br>
+
+*/
 
 export function Sticker({
   text,
