93c51cf96a151372f19cd986a076b7cd5b08479a
Meg Broughton <meg.broughton@americastestkitchen.com>
2020-02-19 15:12:12 -0500

    add Card component

--- src/styles/global.js
+++ src/styles/global.js
@@ -27,6 +27,17 @@ export const globalStyles = css`
     padding: 0;
   }
 
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+    padding: 0;
+  }
+
   ul,
   li {
     list-style-type: none;
