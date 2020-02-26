da2cdeb3f37716dfe7c35e65bbdd4befc5401721
Miranda Chiu <mirandachiu13@gmail.com>
2020-02-26 14:29:21 -0500

    minor updates/cleanup

--- src/components/Badge/Badge.stories.js
+++ src/components/Badge/Badge.stories.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Badge } from '../Badge';
+import { withKnobs, text } from '@storybook/addon-knobs/react';
+import { color } from '../../styles';
 
 export default {
   title: 'Components|Badge',
   component: Badge,
+  decorators: [withKnobs],
 }
 
-export const ATKBadge = () => <Badge type='atk'/>
+export const ATKBadge = () => <Badge type='atk' fill={text('fill', color.transparentBlack)} />
 
-export const CIOBadge = () => <Badge type='cio'/>
+export const CIOBadge = () => <Badge type='cio' fill={text('fill', color.transparentBlack)} />
 
-export const CCOBadge = () => <Badge type='cco'/>
+export const CCOBadge = () => <Badge type='cco' fill={text('fill', color.transparentBlack)} />
 
-export const KidsBadge = () => <Badge type='kids'/>
+export const KidsBadge = () => <Badge type='kids' fill={text('fill', color.transparentBlack)} />
