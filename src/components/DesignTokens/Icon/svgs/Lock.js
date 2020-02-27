69913724f48950386635278a5545f9c0d7e3029d
Meg Broughton <meg.broughton@americastestkitchen.com>
2020-02-27 15:55:31 -0500

    Fix naming collision

--- src/components/DesignTokens/Icon/svgs/Lock.js
+++ src/components/DesignTokens/Icon/svgs/Lock.js
@@ -10,11 +10,11 @@ const Lock = ({ className, fill }) => {
       viewBox="0 0 8 10"
     >
       <defs>
-        <clipPath id="a">
+        <clipPath id="lock-a">
           <path fill={fill} d="M7-5.227H3.993v-1.64a1.41 1.41 0 011.451-1.361h.107A1.41 1.41 0 017-6.868zm1.453 0v-1.641a2.819 2.819 0 00-2.9-2.724h-.109a2.819 2.819 0 00-2.9 2.724v1.641H1.511V.408h8v-5.635z" clipRule="evenodd" transform="translate(-1.511 9.592)"/>
         </clipPath>
       </defs>
-      <g clipPath="url(#a)">
+      <g clipPath="url(#lock-a)">
         <path fill={fill} d="M-6.307-5.913h20.613v21.827H-6.307z"/>
       </g>
     </svg>
