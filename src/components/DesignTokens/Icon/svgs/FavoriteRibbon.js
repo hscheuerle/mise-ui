85fcd968eb18222a90b2e748fe00e19979d6151f
Jason Aslakson <jason.aslakson@americastestkitchen.com>
2020-03-18 10:02:53 -0400

    integration changes and connect changes

--- src/components/DesignTokens/Icon/svgs/FavoriteRibbon.js
+++ src/components/DesignTokens/Icon/svgs/FavoriteRibbon.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { color } from '../../../../styles';
 
 const StyledFavoriteRibbon = styled.svg`
   cursor: pointer;
