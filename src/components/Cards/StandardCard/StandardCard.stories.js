81da1d1f4c23adbbbb97d9aef40c5a998f41db51
Meg Broughton <meg.broughton@americastestkitchen.com>
2020-03-23 15:17:08 -0400

    Improve card story

--- src/components/Cards/StandardCard/StandardCard.stories.js
+++ src/components/Cards/StandardCard/StandardCard.stories.js
@@ -12,13 +12,13 @@ export default {
 export const LoggedIn = () => (
   <StandardCard
     badgeType="atk"
-    contentType="cooking school course"
+    contentType="review"
     commentCount={5}
     ctaText={text("CTA text", "Buy the Winner")}
     ctaUrl="https://www.amazon.com/dp/B01JCNEJSO/?tag=ciosearchresult-20"
     displayFavoritesButton={true}
-    imageAlt={text("Image alt text", "Gray cat pulling up the edge of a rug")}
-    imageUrl={text("Image url", "https://placekitten.com/272/272")}
+    imageAlt={text("Image alt text", "")}
+    imageUrl={text("Image url", "https://res.cloudinary.com/hksqkdlah/image/upload/s--bab2_EML--/c_scale,dpr_2.0,f_auto,h_172,q_auto:low,w_172/40784_sil-food-storage-containers-rubbermaid-brilliance-food-storage-container-large-96-cup-1991158")}
     isFavorited={false}
     displayLockIcon={false}
     stickers={[{type: 'priority', text: 'New'}, {type: 'editorial', text: 'Trending'}]}
@@ -37,8 +37,8 @@ export const LoggedOut = () => (
     displayFavoritesButton={false}
     displayLockIcon={true}
     stickers={[{type: 'priority', text: 'New'}, {type: 'editorial', text: 'Quick'}]}
-    imageAlt={text("Image alt text", "Gray cat pulling up the edge of a rug")}
-    imageUrl="https://placekitten.com/272/272"
+    imageAlt={text("Image alt text", "")}
+    imageUrl="https://res.cloudinary.com/hksqkdlah/image/upload/ar_1:1,c_fill,dpr_2.0,f_auto,fl_lossy.progressive.strip_profile,g_faces:auto,q_auto:low,w_344/SFS_stir_fried_ground_pork_topping_congee-1_1_la51ps"
     displayCommentCount={true}
     title={text("Title", "Congee (Chinese Rice Porridge) with Stir-Fried Ground Pork")}
     url="https://www.cooksillustrated.com/recipes/12381-congee-chinese-rice-porridge-with-stir-fried-ground-pork?extcode=MASCD00L0&ref=new_search_experience_2"
@@ -55,5 +55,6 @@ export const NoImage = () => (
     stickers={[{type: 'editorial', text: 'Make Ahead'}]}
     displayCommentCount={true}
     title={text("Title", "Congee (Chinese Rice Porridge) with Stir-Fried Ground Pork")}
+    url="https://www.cooksillustrated.com/recipes/12381-congee-chinese-rice-porridge-with-stir-fried-ground-pork?extcode=MASCD00L0&ref=new_search_experience_2"
   />
 );
