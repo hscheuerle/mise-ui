d8a6165d7319209bffcd236bcf65152bdf8fa40e
Meg Broughton <meg.broughton@americastestkitchen.com>
2020-03-25 09:37:56 -0400

    fix errors & warning

--- src/components/Cards/shared/FavoriteButton/index.js
+++ src/components/Cards/shared/FavoriteButton/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { color, spacing } from '../../../../styles';
 import { FavoriteRibbon } from '../../../DesignTokens/Icon';
 
