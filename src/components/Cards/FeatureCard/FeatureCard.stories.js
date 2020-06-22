ab459689e347caddff7715e00795a3c752dec939
Meg Broughton <meg.broughton@americastestkitchen.com>
2020-06-22 12:27:50 -0400

    Show only default and wide feature card variations as stories

--- src/components/Cards/FeatureCard/FeatureCard.stories.js
+++ src/components/Cards/FeatureCard/FeatureCard.stories.js
@@ -10,7 +10,7 @@ export default {
   decorators: [withKnobs],
 };
 
-export const Episode = () => (
+export const Default = () => (
   <FeatureCard
     attributions="Episode • Cook's Country"
     contentType="episode"
@@ -27,24 +27,7 @@ export const Episode = () => (
   />
 );
 
-export const Article = () => (
-  <FeatureCard
-    attributions="Article • Cook’s Illustrated"
-    contentType="article"
-    displayFavoritesButton
-    imageAlt={text('Image alt text', '')}
-    imageUrl={text('Image url', 'https://res.cloudinary.com/hksqkdlah/image/upload/v1592418440/mise-play/showstopper-melon-salad.jpg')}
-    siteKey="cio"
-    siteKeyFavorites="cio"
-    stickers={[{ type: 'priority', text: 'New' }]}
-    objectId=""
-    onClick={action('result-click')}
-    title={text('Title', 'Showstopper Melon Salad')}
-    href="https://www.americastestkitchen.com/equipment_reviews/1879-plastic-food-storage-containers?ref=new_search_experience_2"
-  />
-);
-
-export const FeatureCardWide = () => (
+export const Wide = () => (
   <FeatureCard
     attributions="Episode • America's Test Kitchen"
     contentType="episode"
